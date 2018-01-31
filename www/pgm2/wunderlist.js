9acf4e7e1ce9b493107adfa00f5a35787a6da496
marvin78 <marvin1978@gmail.com>
2018-01-31 12:40:08 +0100

    new structure

---
+++ www/pgm2/wunderlist.js
@@ -0,0 +1,241 @@
+﻿if (typeof wunderlist_checkVar === 'undefined') {
+	
+	var wunderlist_checkVar=1;
+
+	var req = new XMLHttpRequest();
+	req.open('GET', document.location, false);
+	req.send(null);
+	var csrfToken = req.getResponseHeader('X-FHEM-csrfToken');
+
+	function wunderlist_encodeParm(oldVal) {
+	    var newVal;
+	    newVal = oldVal.replace(/\$/g, '\\%24');
+	    newVal = newVal.replace(/"/g, '%27');
+	    newVal = newVal.replace(/#/g, '%23');
+	    newVal = newVal.replace(/\+/g, '%2B');
+	    newVal = newVal.replace(/&/g, '%26');
+	    newVal = newVal.replace(/'/g, '%27');
+	    newVal = newVal.replace(/=/g, '%3D');
+	    newVal = newVal.replace(/\?/g, '%3F');
+	    newVal = newVal.replace(/\|/g, '%7C');
+	    newVal = newVal.replace(/\s/g, '%20');
+	    return newVal;
+	};
+
+	function wunderlist_dialog(message) {
+	    $('<div></div>').appendTo('body').html('<div>' + message + '</div>').dialog({
+	        modal: true, title: 'Todoist Error', zIndex: 10000, autoOpen: true,
+	        width: 'auto', resizable: false,
+	        buttons: {
+	            OK: function () {
+	                $(this).dialog("close");
+	            }
+	        },
+	        close: function (event, ui) {
+	            $(this).remove();
+	        }
+	    });
+	    setTimeout(function(){
+				$('.ui-dialog').remove();
+			},10000);
+	};
+	
+	function wunderlist_refreshTable(name,sortit) {
+		var i=1;
+		$('table#wunderlist_' + name + '_table').find('tr').each(function() {
+			// sizes of inputs
+			var input = $(this).find('td.wunderlist_input').find('input[type=text]');
+			var sizeVal = $(this).find('td.wunderlist_input').find('span.wunderlist_task_text').width();
+			var size = sizeVal+5;
+			$(input).width(size);
+			// order
+			var tid = $(this).attr("data-line-id");
+			$(this).removeClass("odd even");
+			if (i%2==0) $(this).addClass("even");
+			else $(this).addClass("odd");
+			if (typeof sortit != 'undefined') wunderlist_sendCommand('set ' + name + ' updateTask ID:'+ tid + ' order="' + i + '"');
+			i++;
+		});
+	}
+
+	function wunderlist_sendCommand(cmd) {
+		var location = document.location.pathname;
+	  if (location.substr(location.length -1, 1) == '/') {
+	      location = location.substr(0, location.length -1);
+	  }
+	  var url = document.location.protocol + "//" + document.location.host + location;
+	  FW_cmd(url + '?XHR=1&fwcsrf=' + csrfToken + '&cmd.' + name + '=' + cmd);
+	}
+
+
+
+	function wunderlist_ErrorDialog($text) {
+		wunderlist_dialog($text);
+	}
+
+	function wunderlist_removeLine(name,id) {
+		var i=1;
+		$('table#wunderlist_' + name + '_table').find('tr').each(function() {
+			var tid = $(this).attr("data-line-id");
+			if (tid==id) $(this).remove();
+			else {
+				$(this).removeClass("odd even");
+				if (i%2==0) $(this).addClass("even");
+				else $(this).addClass("odd");
+				i++;
+			}
+		});
+		wunderlist_getSizes();
+	}
+
+	function wunderlist_addLine(name,id,title) {
+		var lastEl=$('table#wunderlist_' + name + '_table').find('tr').last();
+		var prevEl=$(lastEl).prev('tr');
+		var cl="odd";
+		if (prevEl != 'undefined') {
+			cl = $(prevEl).attr('class');
+			if (cl=="odd") cl="even";
+			else cl="odd"
+		}
+		$(lastEl).before('<tr id="'+ name + "_" + id +'" data-data="true" data-line-id="' + id +'" class="sortit ' + cl +'">\n' +
+	  					'	<td class="col1  wunderlist_col1">\n'+
+	  					'   <div class=\"wunderlist_move\"></div>\n'+
+	  					'		<input class="wunderlist_checkbox_' + name + '" type="checkbox" id="check_' + id + '" data-id="' + id + '" />\n'+
+	  					' </td>\n' +
+	  					'	<td class="col1">\n'+
+	  					' 	<span class="wunderlist_task_text" data-id="' + id + '">' + title + '</span>\n'+
+	  					'   <input type="text" data-id="' + id + '" style="display:none;" class="wunderlist_input_' + name +'" value="' + title + '" />'+
+	  					' </td>\n' +
+	  					' <td class="col2">\n' +
+	  					' 	<a href="#" class="wunderlist_delete_' + name + '" data-id="' + id +'">\n'+
+	  					'			x\n'+
+	  					' 	</a>\n'+
+	  					'	</td>\n'+
+	           	'</tr>\n'
+	  );
+	  wunderlist_refreshTable(name);
+	  wunderlist_getSizes();
+	}
+	
+	function resizable (el, factor) {
+	  var int = Number(factor) || 7.7;
+	  function resize() {el.style.width = ((el.value.length+1) * int) + 'px'}
+	  var e = 'keyup,keypress,focus,blur,change'.split(',');
+	  for (var i in e) el.addEventListener(e[i],resize,false);
+	  resize();
+	}
+
+	
+	function wunderlist_getSizes() {
+		var height = 0;
+		var width = 0;
+		$('.sortable .sortit').each(function() {
+			var tHeight = $(this).outerHeight();
+			if (tHeight > height) height = tHeight;
+		});
+		$('.sortable').css('max-height',height);
+		$('.sortable').css('height',height);
+	}
+
+	$(document).ready(function(){
+		wunderlist_getSizes();
+		$('.wunderlist_name').each(function() {
+			var name = $(this).val();
+			wunderlist_refreshTable(name);
+			$('#wunderlist_' + name + '_table').on('mouseover','tr.sortit',function(e) {
+				$(this).find('div.wunderlist_move').addClass('wunderlist_sortit_handler');
+			});
+			$('#wunderlist_' + name + '_table').on('mouseout','tr.sortit',function(e) {
+				$(this).find('div.wunderlist_move').removeClass('wunderlist_sortit_handler');
+			});
+			$('#newEntry_' + name).on('blur keypress',function(e) {
+				if (e.type!='keypress' || e.which==13) {
+					e.preventDefault();
+					var v=wunderlist_encodeParm($(this).val());
+					if (v!="") {
+						wunderlist_sendCommand('set '+ name +' addTask ' + v);
+						$(this).val("");
+					}
+				}
+			});
+			$('#wunderlist_' + name + '_table').on('click','input[type="checkbox"]',function(e) {
+				var val=$(this).attr('checked');
+				if (!val) {
+					var id=$(this).attr('data-id');
+					wunderlist_sendCommand('set ' + name + ' completeTask ID:'+ id);
+				}
+			});
+			$('#wunderlist_' + name + '_table').on('click','a.wunderlist_delete_'+name,function(e) {
+				if (confirm('Are you sure?')) {
+					var id=$(this).attr('data-id');
+					wunderlist_sendCommand('set ' + name + ' deleteTask ID:'+ id);
+				}
+				return false;
+			});
+			$('#wunderlist_' + name + '_table').on('click','span.wunderlist_task_text',function(e) {
+				var id = $(this).attr("data-id");
+				var val=$(this).html();
+				var width=$(this).width()+20;
+				$(this).hide();
+				$("input[data-id='" + id +"']").show().focus().val("").val(val);
+			});
+			$('#wunderlist_' + name + '_table').on('blur keypress','input.wunderlist_input_'+name,function(e) {
+				if (e.type!='keypress' || e.which==13) {
+					e.preventDefault();
+					var val = $(this).val();
+					
+					var comp = $(this).prev().html();
+					var id = $(this).attr("data-id");
+					var val = $(this).val();
+					$(this).hide();
+					$("span.wunderlist_task_text[data-id='" + id +"']").show();
+					if (val != "" && comp != val) {
+						$("span.wunderlist_task_text[data-id='" + id +"']").html(val);
+						wunderlist_sendCommand('set ' + name + ' updateTask ID:'+ id + ' title="' + val + '"');
+					}
+					
+					if (val == "" && e.which==13) {
+						if (confirm('Are you sure?')) {
+							$('#newEntry_' + name).focus();
+							wunderlist_sendCommand('set ' + name + ' deleteTask ID:'+ id);
+						}
+					}
+				}
+				if (e.type=='keypress') {
+					resizable(this,7);
+				}
+			});
+		});
+		var fixHelper = function(e, ui) {  
+		  ui.children().each(function() {  
+		  console.log(e);
+		    $(this).width($(this).width());  
+		  });  
+		  return ui;  
+		};
+		$( ".sortable" ).sortable({
+			axis: 'y',
+			revert: true,
+			items: "> tbody > tr.sortit",
+			handle: ".wunderlist_sortit_handler",
+			forceHelperSize: true,
+			placeholder: "sortable-placeholder",
+			helper: fixHelper,
+			start: function( event, ui ) { 
+				ui.item.css('background','#111111');
+				var width = ui.item.innerWidth();
+				ui.placeholder.css("width",width); 
+				var height = ui.item.innerHeight();
+				ui.placeholder.css("height",height); 
+			},
+			stop: function (event,ui) {
+				var parent = ui.item.parent().parent();
+				var id = $(parent).attr('id');
+				var name = id.split("_")[1];
+				ui.item.css('background','');
+				wunderlist_refreshTable(name,1);
+			}
+		}).disableSelection();
+	});
+
+}
